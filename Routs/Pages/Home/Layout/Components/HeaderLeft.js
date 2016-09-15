82e65fbb15d4be6e71db9adbfa2f45f920544eef
Jason Clifford <jason@ocmedia.ca>
2016-09-15 08:43:47 -0600

    second commit to test myself

--- Routs/Pages/Home/Layout/Components/HeaderLeft.js
+++ Routs/Pages/Home/Layout/Components/HeaderLeft.js
@@ -4,7 +4,7 @@ export default class HeaderLeft extends React.Component {
     render(){
       return (
         <div id="LeftBar1">
-          <h1>It works</h1>
+          <h1>It workssss</h1>
         </div>
       );
     }
