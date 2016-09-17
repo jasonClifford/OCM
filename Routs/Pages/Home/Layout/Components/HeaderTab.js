7d21aaa31e5f27fb0479c9bfa64f18f9cd509f55
Jason Clifford <jason@ocmedia.ca>
2016-09-17 12:40:19 -0600

    Just got React motion working Yaaaaaaygit add .

--- Routs/Pages/Home/Layout/Components/HeaderTab.js
+++ Routs/Pages/Home/Layout/Components/HeaderTab.js
@@ -1,11 +1,18 @@
 import React from "react";
+import {Motion, spring} from 'react-motion';
 
 export default class HeaderTab extends React.Component {
   render(){
       return (
+        <div id="MenuTab">
 
-          <div id="MenuTab">
-            //code for the menu and logo +
+
+          <Motion defaultStyle={{x: 0}} style={{x: spring(300)}}>
+              {value => <h1>{value.x}</h1>}
+            </Motion>
+
+
+
 
           </div>
       );
