069d74f76cbbb91258357942819a5322a023ca1f
Jason Clifford <jason@ocmedia.ca>
2016-09-18 11:29:51 -0600

    App with react motion working 0-300numbers

--- Routs/Pages/Home/Layout/Components/HeaderLeft.js
+++ Routs/Pages/Home/Layout/Components/HeaderLeft.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Motion, spring} from 'react-motion';
 
 export default class HeaderLeft extends React.Component {
   _MenuOpen(){
@@ -13,7 +14,12 @@ export default class HeaderLeft extends React.Component {
             <div id="MenuBtn" onClick={this._MenuOpen.bind(this)}></div>
 
          </div>
-         <div id="MenuTab"></div>
+         <div id="MenuTab">
+           <Motion defaultStyle={{x: 0}} style={{x: spring(10)}}>
+              {value => <h1>{value.x}</h1>}
+            </Motion>
+
+         </div>
         </div>
       );
     }
