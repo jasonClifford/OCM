0e480f5dce0db7f6e5e6d378ddd8ff3b796b7e7e
Jason Clifford <jason@ocmedia.ca>
2016-09-18 11:26:50 -0600

    resave no react motion working

--- Routs/Pages/Home/Layout/Components/HeaderLeft.js
+++ Routs/Pages/Home/Layout/Components/HeaderLeft.js
@@ -7,11 +7,13 @@ export default class HeaderLeft extends React.Component {
 
     render(){
       return (
-        <div id="MenuBar1">
-          <div id="MenuBtn" onClick={this._MenuOpen.bind(this)} ></div>
-        
+        <div>
+          <div id="MenuBar1">
 
+            <div id="MenuBtn" onClick={this._MenuOpen.bind(this)}></div>
 
+         </div>
+         <div id="MenuTab"></div>
         </div>
       );
     }
--- Routs/Pages/Home/Layout/Layout.js
+++ Routs/Pages/Home/Layout/Layout.js
@@ -1,13 +1,12 @@
 import React from "react";
 import HeaderLeft from "./Components/HeaderLeft.js";
-import HeaderTab from "./Components/HeaderTab.js";
+
 
 export default class Layout extends React.Component {
     render(){
       return (
         <div>
           <HeaderLeft />
-          <HeaderTab />
         </div>
       );
     }
