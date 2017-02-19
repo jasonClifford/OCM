07a384888f6df2ac5df3a2435e07999ec41253a2
Jason Clifford <jason@ocmedia.ca>
2017-02-19 11:56:10 -0700

    added a toggle button on home page for the weather toggle

--- Routs/Contact.js
+++ Routs/Contact.js
@@ -2,6 +2,8 @@ import React from "react";
 
 
 export default class Contact extends React.Component {
+
+
     render(){
       return (
         <div className="mainContainer">
--- Routs/Pages/Home/Components/Precip.js
+++ Routs/Pages/Home/Components/Precip.js
@@ -1,21 +1,40 @@
 import React from 'react';
+import $ from 'jquery';
 
 
+export default class Precip extends React.Component {
+    constructor() {
+        super();
+        this.toggleState = this.toggleState.bind(this);
+        this.state = {
+            isActive: false
 
-const Precip = React.createClass({
-
-
-
-  render() {
+        }
+    }
+    toggleState() {
+        this.setState({ isActive: !this.state.isActive });
+        //this.BtnOnOff();
+       // console.log(this.state.isActive);
+    } 
+    
+    
+      render() {
           return (
-            <div>
+           <div className="precipPage">
+                  
+                  <label id="Switch" >
+                      <input id="ONOFF" type="checkbox" className={this.state.isActive ? 'inactive' : 'active'} onClick={this.toggleState}></input>
+                      <div className="slider round"></div>
+                  </label>
 
+                  <div id="ip_content" />
+                  <h2 id="place"></h2>
+                  <h1 id="weather"></h1>
 
-
-            <h1> Im Precip Page </h1>
+                  <canvas id="weathercanvas" />
 
             </div>
           );
       }
-});
-export default Precip;
+}
+
